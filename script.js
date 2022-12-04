da4a7abedfe958fb4480d1518b2ed106684df86b
lane30schmidt <111534250+lane30schmidt@users.noreply.github.com>
2022-12-03 19:05:39 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+$(function(){
+  $('#console-box').append('console is ready...');
+  
+  $('button').click(function(){
+    $('#console-box').append('<br>class:'+$(this).attr("class")+'|value:'+$(this).text()+'.');
+    $(this).toggleClass('selected').siblings().removeClass('selected');
+; 
+  });
+  
+});
+  
+
